db0bb9e76233f7d89d088e8ff987dcf07f786d52
Nurmx7292 <raxmetnur04@gmail.com>
2025-06-24 01:43:19 +0200

    Add icons as static files

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,26 +1,27 @@
 import React from 'react'
 import classes from "./Header.module.css"
+import Menu from "../../assets/Menu.svg"
 export const Header = () => {
   return (
     <header className={classes.header}>
-            <div className="logo">
+            <div className={classes.logo}>
                 <button>
-                    <img src="assets/Menu.svg"/>
+                    <img src={Menu} alt='#'/>
                 </button>
                 <a href="/">
                     <img src="assets/Logo_mobile.svg" alt="YouTube Logo"/>
                     <img src="assets/youtubeLogo.svg" alt="YouTube Logo Text"/>
                 </a>
             </div>
-            <form action="#" className="search-bar">
+            <form action="#" className={classes["search-bar"]}>
                 <input type="text" name="search" id="search" placeholder="Search" aria-label="Search query"/>
-                <button className="search" aria-label="Search"><img src="assets/search.svg" alt="#"/></button>
+                <button className={classes.search} aria-label="Search"><img src="assets/search.svg" alt="#"/></button>
             </form>
-            <div className="header-actions">
+            <div className={classes["header-actions"]}>
                 <button><img src="assets/video.svg"/></button>
                 <button><img src="assets/more.svg"/></button>
                 <button><img src="assets/notification.svg"/></button>
-                <img className="profile-pic" src="https://picsum.photos/40/40" alt="Profile image"/>
+                <img className={classes["profile-pic"]} src="https://picsum.photos/40/40" alt="Profile image"/>
             </div>
         </header>
   )
