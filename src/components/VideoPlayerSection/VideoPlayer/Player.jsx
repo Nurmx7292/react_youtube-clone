ef94de1dc91a67c6e724cce8a6c0528f0ffa4aff
Nurmx7292 <raxmetnur04@gmail.com>
2025-06-24 14:34:02 +0200

    feat: implement the bottom controls, and finish the player window

--- src/components/VideoPlayerSection/VideoPlayer/Player.jsx
+++ src/components/VideoPlayerSection/VideoPlayer/Player.jsx
@@ -8,6 +8,16 @@ export const Player = () => {
         <img className={classes.cover} src={Cover} alt="#" />
         <div className={classes.controls}>
             <ProgressBar/>
+            <div className={classes.bottomControls}>
+                <button><img src="assets/Pause.svg" alt="#" /></button>
+                <button><img src="assets/Next.svg" alt="#" /></button>
+                <button><img src="assets/Volume.svg" alt="#" /></button>
+                <input type="range" className={classes.volume} />
+                <button className={classes.subtitles}><img src="assets/Subtitles.svg" alt="#"/></button>
+                <button><img src="assets/settings.svg" alt="#" /></button>
+                <button><img src="assets/Size.svg" alt="#" /></button>
+                <button><img src="assets/FullScreen.svg" alt="#" /></button>
+            </div>
         </div>
     </div>
   )
