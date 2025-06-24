0c7d9ad443d1142242c4be3d566231cbf41a59c8
Nurmx7292 <raxmetnur04@gmail.com>
2025-06-24 18:37:29 +0200

    Add resize handler to make mobile player controls appear

--- src/components/VideoPlayerSection/VideoPlayer/Player.jsx
+++ src/components/VideoPlayerSection/VideoPlayer/Player.jsx
@@ -1,13 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import classes from "./VideoPlayer.module.css"
 import Cover from "../../../assets/Cover.png"
 import { ProgressBar } from '../../UI/ProgressBar/ProgressBar'
 export const Player = () => {
+    const [width,setWidth] = useState(window.innerWidth)
+    useEffect(()=>{
+        window.addEventListener("resize",()=>setWidth(window.innerWidth))
+    })
   return (
     <div className={classes.player}>
         <img className={classes.cover} src={Cover} alt="#" />
         <div className={classes.controls}>
-            <ProgressBar duration={"3:22"} currentTime={"1:22"}/>
+            {width>900 && <ProgressBar duration={"3:22"} currentTime={"1:22"}/>}
+            <div className={classes.mobileControls}>
+                <button><img src="assets/Pause.svg" alt="#" /></button>
+                <ProgressBar duration={"10:00"} currentTime={"1:34"} />
+                <button><img src="assets/Volume.svg" alt="#" /></button>
+            </div>
             <div className={classes.bottomControls}>
                 <button><img src="assets/Pause.svg" alt="#" /></button>
                 <button><img src="assets/Next.svg" alt="#" /></button>
